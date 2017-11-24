354091c04448de758514865a994972316c7a8895
LucaCarrarini <32509417+LucaCarrarini@users.noreply.github.com>
2017-11-24 09:22:44 +0100

    Create robott.ino

---
+++ robott.ino
@@ -0,0 +1,38 @@
+const int trig_pin = 9;
+const int echo_pin = 10;
+
+void setup() {
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  Serial.begin(9600);
+}
+
+//echo rimane in HIGH per tutto il tempo di durata del viaggio delle onde
+//v=343 m/s -> v=0,0343 cm/micros
+//s[cm]=0,0343 [cm/micros] * t [micros] * 1/2
+long calcola_distanza(t){
+  long s = (0.0343)*(t)*(0.5);
+  return s;
+}
+
+void loop() {
+  digitalWrite(trig_pin, LOW);
+  delay(10);
+  //impulso di durata 10us sul trigger come da datasheet
+  digitalWrite(trig_pin, HIGH);
+  delay(10);
+  digitalWrite(trig_pin, LOW);
+    
+  //OSS:oltre i 20cm il sensore risulta poco preciso allora pongo come tempo limite 1200 microsecondi, iin modo che più o meno oltre i 20 cm risulti fuori portata e non valori poco precisi
+  
+  long t = pulseIn(echo_pin, HIGH, 50000);
+  long s = calcola_distanza(t);
+  Serial.print(s);
+  if(t==0){
+    Serial.println("Fuori portata");
+  }
+  else{
+    Serial.println("distanza[cm]:");
+    Serial.println(s);
+  }
+}
